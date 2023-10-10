9ea8dcad96df6dba42ad94b31f3047994338545a
Henry Nawrocki Sztanski <henry@sztanski.com>
2023-10-09 22:38:21 -0500

    Modify tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
-}
+    './app/javascript/**/*.js',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1aa8db',
+        secondary: '#d23838',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
