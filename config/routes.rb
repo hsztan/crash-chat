de10c31642c9eb806d81212c9406d822f8054cc1
Henry Nawrocki Sztanski <henry@sztanski.com>
2023-10-11 13:49:10 -0500

    Add chat homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  devise_for :users
+  root 'pages#home'
+  resources :messages, only: %i[create destroy]
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
